2ed8e332e15d4287f2e27e0a29d9e3eca9666182
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2022-04-23 15:54:57 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+--[[
+
+				██╗  ██╗   ██╗██╗  ██╗ ██████╗ ███████╗
+				██║  ╚██╗ ██╔╝╚██╗██╔╝██╔═══██╗██╔════╝
+				██║   ╚████╔╝  ╚███╔╝ ██║   ██║███████╗
+				██║    ╚██╔╝   ██╔██╗ ██║   ██║╚════██║
+				███████╗██║   ██╔╝ ██╗╚██████╔╝███████║
+				╚══════╝╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+					Developer: zImSkillz#4906
+						> Let's Ban the Cheater
+]]--
+
+
+fx_version 'adamant'
+games {'gta5'}
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'Configs/Config-Client.lua',
+    'Client/*.lua',
+}
+
+server_scripts {
+    'Configs/Config-Server.lua',
+    'Server/*.lua',
+}
+
+files {
+	'Web/*.html',
+	'Web/js/*.js'
+}
