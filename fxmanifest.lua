96be649924a8ac8f2f9e099b32823c1067ef62b6
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2022-05-25 20:27:22 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,11 +10,14 @@
 						> Let's Ban the Cheater
 ]]--
 
+-- TEST
 
 fx_version 'cerulean'
 games { 'gta5' }
 
 author 'zImSkillz'
+
+shared_script 'Shared/EventProtection.lua'
 
 client_scripts {
     '@menuv/menuv.lua',
@@ -23,6 +26,7 @@ client_scripts {
 }
 
 server_scripts {
+    '@mysql-async/lib/MySQL.lua',
     'Configs/Config-Server.lua',
     'Server/*.lua'
 }
@@ -33,7 +37,6 @@ files {
 }
 
 escrow_ignore {
-  'Bans.json',
   'Configs/*.lua',
   'Web/*.*',
   'Web/js/*.*'
